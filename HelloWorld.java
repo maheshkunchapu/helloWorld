ce92987bd8d61761f7e1b9a0810dbbc31c7c76fa
maheshkunchapu <kunchapumahesh12@gmail.com>
2020-08-17 20:47:40 +0000

    wirtten code for new project HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//writing code for new project"HelloWorld.java"
+/* HelloWorld.java
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+} 
